58267451e4ebd8c31d90b91285762e6e0d48e882
vctrthe <victorthe123@gmail.com>
2026-09-22 14:44:03 +0700

    chapt16: strings part 2

--- MyTool/main.swift
+++ MyTool/main.swift
@@ -20,11 +20,50 @@ let multilineQuote = """
 //print(multilineQuote)
 let einsteinQuote = "\"Learn from yesterday, Live for today, Hope for tomorrow.\" – Albert Einstein"
 
-print(einsteinQuote)
+//print(einsteinQuote)
 
 // Raw string
 let einsteinQuoteRaw = #""Learn from yesterday, Live for today, Hope for tomorrow." – Albert Einstein"#
 
-print(einsteinQuoteRaw)
+//print(einsteinQuoteRaw)
+//
+//print(einsteinQuote == einsteinQuoteRaw)
 
-print(einsteinQuote == einsteinQuoteRaw)
+// Empty string
+let emptyString = ""
+let emptyString2 = String()
+let someString = "Pizza 🍕"
+
+if emptyString.isEmpty {
+    print("The given string is empty")
+} else {
+    print(#"The given string "\#(someString.lowercased())" is not empty"#)
+}
+
+// Counting
+if someString.count == 1 {
+    print("The string '\(someString)' has \(someString.count) character")
+} else {
+    print("The string '\(someString)' has \(someString.count) characters")
+}
+
+// Alternative approach
+var msg = "The string '\(someString)' has \(someString.count) character"
+if someString.count != 1 {
+    msg += "s" // Pluralize
+}
+
+print(msg)
+
+// ternary operator
+// a ? b : c
+let plural = someString.count == 1 ? "" : "s"
+print("The string '\(someString)' has \(someString.count) character\(plural)")
+
+// Loop over a string
+for ch in someString {
+    print(ch)
+}
+
+print(someString.dropLast())
+let x: String = String(someString.dropLast())
