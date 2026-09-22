6ac94184e4a5ce48819952f76b604f3517bd0dc4
vctrthe <victorthe123@gmail.com>
2026-09-22 14:25:50 +0700

    chapt15: introduction on strings

--- MyTool/main.swift
+++ MyTool/main.swift
@@ -1,3 +1,30 @@
 import Foundation
 
-print("Hello, World!")
+print("Strings Topic")
+
+// Example of a string literal – escape sequence
+//let quote = "Learn from\nyesterday,\n\nLive for today,\n\nHope for\ntomorrow."
+//
+//print(quote)
+
+let multilineQuote = """
+\tLearn from
+\tyesterday,
+
+\tLive for today,
+
+\tHope for,
+\ttomorrow.
+"""
+
+//print(multilineQuote)
+let einsteinQuote = "\"Learn from yesterday, Live for today, Hope for tomorrow.\" – Albert Einstein"
+
+print(einsteinQuote)
+
+// Raw string
+let einsteinQuoteRaw = #""Learn from yesterday, Live for today, Hope for tomorrow." – Albert Einstein"#
+
+print(einsteinQuoteRaw)
+
+print(einsteinQuote == einsteinQuoteRaw)
